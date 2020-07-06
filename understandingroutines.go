b3e48d26238ecbd86cf9b73c09f6bb73a604cc5a
Chaitanya Deepika <54633736+deepikakc34@users.noreply.github.com>
2020-07-06 15:02:59 -0400

    Update understandingroutines.go

--- understandingroutines.go
+++ understandingroutines.go
@@ -2,18 +2,17 @@ package main
 
 import (
 		"fmt"
-	//	"time"
 )
 
 func modif() {
 	fmt.Println("in first go routine function")
-	//time.Sleep(10 * 1e9)
+
 	fmt.Println("ending first go routine")
 }
 
 func prtn() {
 	fmt.Println("in second go routine function")
-//	time.Sleep(10 * 1e9)
+
 
 	fmt.Println("ending second go routine function")
 }
@@ -24,7 +23,6 @@ func main(){
 	fmt.Println("in main function")
 	go modif()
 	go prtn()
-	//time.Sleep(10 * 1e9)
 	fmt.Println("ending main function")
 	
 }
